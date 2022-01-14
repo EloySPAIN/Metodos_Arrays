8b212b379adb65f392d0f523504104e3fdeb1955
EloyJordiJaumeAndy <ealtozano@institutjaumehuguet.cat>
2022-01-14 19:29:22 +0100

    Metodos_Arrays 12 Eloy Altozano Meijer

--- src/MetodoDoceApp.java
+++ src/MetodoDoceApp.java
@@ -1,9 +1,39 @@
-
+/**
+ * 
+ * @author Eloy Altozano Meijer
+ *
+ */
+import java.util.Scanner;
+import java.lang.Math;
 public class MetodoDoceApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Escribe la longtiud del array: ");
+		Scanner scan = new Scanner(System.in);
+		int parametro=scan.nextInt();
+		int array[]=new int[parametro];
+		rellenar(array);
+		mostrarEnd(array);
+		}
+	
+	public static void rellenar(int array[]) {
+		System.out.println("Todo el Array:");
+		for(int i=0;i<array.length;i++) {
+			array[i]=(int) (Math.random()*300);
+			System.out.println(array[i]);
+		}
+	}
+	
+	public static void mostrarEnd(int array[]) {
+		System.out.println("Escribe con que numero acabar: ");
+		Scanner num = new Scanner(System.in);
+		String numFinal=num.nextLine();
+		for(int i=0;i<array.length;i++) {
+			String numString=String.valueOf(array[i]);
+			if(numString.endsWith(numFinal)) {
+				System.out.println(array[i]);
+			}
+		}
 	}
 
 }
